491023f5a8da17b0c41ff2db534d5689dd4999fd
Luis del Amo <luisdelamo@gmail.com>
2017-03-14 00:47:40 +0000

    se crea archivo del behavior

---
+++ src/Model/Behavior/MessagesBehaviorBehavior.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Model\Behavior;
+
+use Cake\ORM\Behavior;
+use Cake\ORM\Table;
+
+/**
+ * MessagesBehavior behavior
+ */
+class MessagesBehaviorBehavior extends Behavior
+{
+
+    /**
+     * Default configuration.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [];
+}
